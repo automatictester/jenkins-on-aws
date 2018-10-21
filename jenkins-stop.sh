001236bc60d43bfb73be04435603d0ab7804dce1
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-10-21 11:44:22 +0100

    Update Jenkins slave instance type

--- jenkins-stop.sh
+++ jenkins-stop.sh
@@ -7,7 +7,7 @@ echo -n 'Stopping Jenkins master... '
 aws ec2 stop-instances --instance-ids ${JENKINS_MASTER_INSTANCE_ID} --output text >> /dev/null
 echo 'done!'
 
-JENKINS_SLAVE_INSTANCE_ID=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=jenkins slave - t3.small' 'Name=instance-state-name,Values=running' --query 'Reservations[*].Instances[*].InstanceId' --output text)
+JENKINS_SLAVE_INSTANCE_ID=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=jenkins slave - t3.medium' 'Name=instance-state-name,Values=running' --query 'Reservations[*].Instances[*].InstanceId' --output text)
 echo "Jenkins slave instance ID: ${JENKINS_SLAVE_INSTANCE_ID}"
 
 echo -n 'Terminating Jenkins slave, if any... '
