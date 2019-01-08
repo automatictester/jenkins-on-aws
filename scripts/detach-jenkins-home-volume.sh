ae50ed6546f1abe8fd2314620871c09aee1bee7f
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-01-08 17:47:49 +0000

    Wait for volume to get detached

--- scripts/detach-jenkins-home-volume.sh
+++ scripts/detach-jenkins-home-volume.sh
@@ -6,4 +6,9 @@ JENKINS_HOME_VOLUME_ID=$(aws ec2 describe-volumes \
     --query 'Volumes[0].VolumeId'
 )
 
+echo "Detaching volume..."
+
 aws ec2 detach-volume --volume-id ${JENKINS_HOME_VOLUME_ID}
+aws ec2 wait volume-available --volume-ids ${JENKINS_HOME_VOLUME_ID}
+
+echo "Volume detached"
