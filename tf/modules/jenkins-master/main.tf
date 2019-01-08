ea2840924b2c065cb163bc7cf364b15c17176d99
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-01-08 12:43:59 +0000

    Introduce TF modules

---
+++ tf/modules/jenkins-master/main.tf
@@ -0,0 +1,68 @@
+resource "aws_iam_role" "jenkins_master_role" {
+  name                 = "JenkinsMaster"
+  assume_role_policy   = "${file("iam-policy/assume-role-policy.json")}"
+}
+
+resource "aws_iam_policy" "pass_role_policy" {
+  name                 = "PassRole"
+  path                 = "/"
+  policy               = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "jenkins_master_manage_ec2_slaves_policy" {
+  name                 = "JenkinsMasterManageEC2Slaves"
+  path                 = "/"
+  policy               = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:GetConsoleOutput",
+                "ec2:RunInstances",
+                "ec2:StartInstances",
+                "ec2:StopInstances",
+                "ec2:TerminateInstances",
+                "ec2:CreateTags",
+                "ec2:DeleteTags",
+                "ec2:DescribeInstances",
+                "ec2:DescribeKeyPairs",
+                "ec2:DescribeRegions",
+                "ec2:DescribeImages",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "pass_role_policy_attachment" {
+  role                 = "${aws_iam_role.jenkins_master_role.name}"
+  policy_arn           = "${aws_iam_policy.pass_role_policy.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "manage_ec2_slaves_policy_attachment" {
+  role                 = "${aws_iam_role.jenkins_master_role.name}"
+  policy_arn           = "${aws_iam_policy.jenkins_master_manage_ec2_slaves_policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "jenkins_master_instance_profile" {
+  name                 = "JenkinsMaster"
+  role                 = "${aws_iam_role.jenkins_master_role.name}"
+}
---
+++ tf/modules/jenkins-slave/main.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "jenkins_slave_role" {
+  name                 = "JenkinsSlave"
+  assume_role_policy   = "${file("iam-policy/assume-role-policy.json")}"
+}
+
+resource "aws_iam_policy" "jenkins_slave_manage_iam_through_tf_policy" {
+  name                 = "JenkinsSlaveManageIAMThroughTF"
+  path                 = "/"
+  policy               = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreatePolicy",
+                "iam:DetachRolePolicy",
+                "iam:ListPolicyVersions",
+                "iam:ListAttachedRolePolicies",
+                "iam:UpdateRoleDescription",
+                "iam:DeletePolicy",
+                "iam:CreateRole",
+                "iam:DeleteRole",
+                "iam:UpdateRole",
+                "iam:AttachRolePolicy",
+                "iam:CreatePolicyVersion"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_policy_attachment" {
+  role                 = "${aws_iam_role.jenkins_slave_role.name}"
+  policy_arn           = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_full_access_policy_attachment" {
+  role                 = "${aws_iam_role.jenkins_slave_role.name}"
+  policy_arn           = "arn:aws:iam::aws:policy/AWSLambdaFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "manage_iam_through_tf_policy_attachment" {
+  role                 = "${aws_iam_role.jenkins_slave_role.name}"
+  policy_arn           = "${aws_iam_policy.jenkins_slave_manage_iam_through_tf_policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "jenkins_slave_instance_profile" {
+  name                 = "JenkinsSlave"
+  role                 = "${aws_iam_role.jenkins_slave_role.name}"
+}
