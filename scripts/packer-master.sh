a4c9a4ed089b1334d47c8c703b2c1d80522aaee3
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-05-19 13:07:37 +0200

    Launch Packer instance into Jenkins subnet

--- scripts/packer-master.sh
+++ scripts/packer-master.sh
@@ -2,8 +2,24 @@
 
 JENKINS_HOME_VOLUME_ID=$(aws ec2 describe-volumes \
     --filters 'Name=tag:Name,Values=JENKINS_HOME' \
-    --output text \
-    --query 'Volumes[0].VolumeId'
+    --query 'Volumes[0].VolumeId' \
+    --output text
 )
 
-packer build -var "jenkins_home_volume_id=${JENKINS_HOME_VOLUME_ID}" ../packer/jenkins-master.json
+JENKINS_VPC_ID=$(aws ec2 describe-vpcs \
+    --filters 'Name=tag:Name,Values=Jenkins VPC' \
+    --query 'Vpcs[0].VpcId' \
+    --output text
+)
+
+JENKINS_SUBNET_ID=$(aws ec2 describe-subnets \
+    --filters 'Name=tag:Name,Values=Jenkins Subnet' \
+    --query 'Subnets[0].SubnetId' \
+    --output text
+)
+
+packer build \
+    -var "jenkins_home_volume_id=${JENKINS_HOME_VOLUME_ID}" \
+    -var "jenkins_vpc_id=${JENKINS_VPC_ID}" \
+    -var "jenkins_subnet_id=${JENKINS_SUBNET_ID}" \
+    ../packer/jenkins-master.json
